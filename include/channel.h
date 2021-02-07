ab737a70b199a7a7be0d92f6761557482730d337
zhangxiaorui <923273174@qq.com>
2021-02-07 16:51:51 +0800

    modify channel.h

--- include/channel.h
+++ include/channel.h
@@ -2,27 +2,30 @@
 #define CHANNEL_H_
 
 #include <iostream>
+#include <functional>
 #include <memory.h>
 #include <utility>
 #include "poller.h"
 #include "util.h"
 
-struct Channel : private noncopyable {
+typedef std::function<void()> Task;
+struct Channel : private handy::noncopyable {
     // base为事件管理器，fd为通道内部的fd，events为通道关心的事件
     Channel(int fd, int events);
-    ~Channel();
-    int fd() { return fd_; }
-    //通道id
-    int64_t id() { return id_; }
-    short events() { return events_; }
-    //关闭通道
+    virtual ~Channel();
+
+    int fd() const { return fd_; }
+    // 通道 id
+    int64_t id() const { return id_; }
+    short events() const { return events_; }
+    // 关闭通道
     void close();
 
-    //挂接事件处理器
-    void onRead(const auto &readcb) { readcb_ = readcb; }
-    void onWrite(const auto &writecb) { writecb_ = writecb; }
-    void onRead(auto &&readcb) { readcb_ = readcb; }
-    void onWrite(auto &&writecb) { writecb_ = writecb; }
+    // 挂接事件处理器
+    void onRead(const Task &readcb) { readcb_ = readcb; }
+    void onWrite(const Task &writecb) { writecb_ = writecb; }
+    void onRead(Task &&readcb) { readcb_ = readcb; }
+    void onWrite(Task &&writecb) { writecb_ = writecb; }
 
     //启用读写监听
     void enableRead(bool enable){
@@ -42,31 +45,31 @@ struct Channel : private noncopyable {
         poller_->updateChannel(this);
     }
     void enableReadWrite(bool readable, bool writable){
-        if (enable) {
+        if (readable) {
             events_ |= EPOLLIN;
         } else {
             events_ &= ~EPOLLIN;
         }
-        if (enable) {
+        if (writable) {
             events_ |= EPOLLOUT;
         } else {
             events_ &= ~EPOLLOUT;
         }
         poller_->updateChannel(this);
-    };
+    }
     bool getReadStatus(){ return events_ & EPOLLIN; }
-    bool getWriteStatus(){ return events_ & EPOLLOUT; };
+    bool getWriteStatus(){ return events_ & EPOLLOUT; }
 
     //处理读写事件
     void handleRead() { readcb_(); }
     void handleWrite() { writecb_(); }
 
    protected:
-    PollerBase *poller_;
+    handy::PollerBase *poller_;
     int fd_;
     short events_;
     int64_t id_;
-    std::function<void()> readcb_, writecb_, errorcb_;
+    Task readcb_, writecb_, errorcb_;
 };
 
 #endif
